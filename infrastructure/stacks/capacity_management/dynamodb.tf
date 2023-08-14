66898a6ae44087f5eb8b692f06a18d6b12a56d29
pira Maheswaran <piravinth.maheswaran1@nhs.net>
2023-08-14 13:11:35 +0100

    DR-330 Dynamodb infra deploy

--- infrastructure/stacks/capacity_management/dynamodb.tf
+++ infrastructure/stacks/capacity_management/dynamodb.tf
@@ -1,67 +1,36 @@
 module "dynamodb_organisations_table" {
   source = "../../modules/dynamodb"
 
-  table_name          = var.organisations_table_name
-  hash_key            = "id"
-  autoscaling_enabled = true
-  stream_enabled      = true
-  stream_view_type    = "NEW_AND_OLD_IMAGES"
+  table_name = "organisations"
+}
 
-  attributes = [
-    {
-      name = "id"
-      type = "S"
-    }
-  ]
+module "dynamodb_organisation_affiliation_table" {
+  source = "../../modules/dynamodb"
+
+  table_name = "organisation_affiliation"
 }
 
 module "dynamodb_locations_table" {
   source = "../../modules/dynamodb"
 
-  table_name          = var.locations_table_name
-  hash_key            = "id"
-  autoscaling_enabled = true
-  stream_enabled      = true
-  stream_view_type    = "NEW_AND_OLD_IMAGES"
-
-  attributes = [
-    {
-      name = "id"
-      type = "S"
-    }
-  ]
+  table_name = "locations"
 }
 
 module "dynamodb_healthcare_services_table" {
   source = "../../modules/dynamodb"
 
-  table_name          = var.healthcare_services_table_name
-  hash_key            = "id"
-  autoscaling_enabled = true
-  stream_enabled      = true
-  stream_view_type    = "NEW_AND_OLD_IMAGES"
-
-  attributes = [
-    {
-      name = "id"
-      type = "S"
-    }
-  ]
+  table_name = "healthcare_services"
 }
 
-module "dynamodb_sdc_table" {
+module "dynamodb_questionnaire_table" {
   source = "../../modules/dynamodb"
 
-  table_name          = var.sdc_table_name
-  hash_key            = "id"
-  autoscaling_enabled = true
-  stream_enabled      = true
-  stream_view_type    = "NEW_AND_OLD_IMAGES"
-
-  attributes = [
-    {
-      name = "id"
-      type = "S"
-    }
-  ]
+  table_name = "questionnaire"
 }
+
+module "dynamodb_questionnaire_response_table" {
+  source = "../../modules/dynamodb"
+
+  table_name = "questionnaire_response"
+}
+
