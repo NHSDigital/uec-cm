a35b20b8df2f9bae708c5472464e148f276eaff1
Tim Rickwood <tim.rickwood@nhs.net>
2023-08-15 13:16:03 +0100

    DR-336 Move stack post rebase

--- infrastructure/stacks/capacity_management/variables.tf
+++ infrastructure/stacks/capacity_management/variables.tf
@@ -7,3 +7,27 @@ variable "project_owner" {
 variable "environment" {
   description = "The environment - dev, test, staging etc"
 }
+variable "domain_name" {
+  description = "Domain name"
+}
+variable "throttle_burst_limit" {
+  description = "Burst limit for throttling"
+}
+variable "throttle_rate_limit" {
+  description = "Rate limit for throttling"
+}
+variable "detailed_metrics_enabled" {
+  description = "Enable or disable detailed metrics"
+}
+variable "logs_retention_days" {
+  description = "Number of days to retain logs"
+}
+variable "truststore_bucket_name" {
+  description = "Name of bucket holding trust store for api gateway"
+}
+variable "cognito_pool_name" {
+  description = "Name of cognito user pool"
+}
+variable "authorizer_name" {
+  description = "Name of authorizer for api gateway"
+}
