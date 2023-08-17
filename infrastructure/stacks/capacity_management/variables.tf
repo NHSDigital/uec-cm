6751c29d3b9ef7376aab907570774ff6eae80b99
Tim Rickwood <tim.rickwood@nhs.net>
2023-08-17 09:52:37 +0100

    DR-336 Remove temp stack

--- infrastructure/stacks/capacity_management/variables.tf
+++ infrastructure/stacks/capacity_management/variables.tf
@@ -7,3 +7,15 @@ variable "project_owner" {
 variable "environment" {
   description = "The environment - dev, test, staging etc"
 }
+variable "api_gateway_name" {
+  description = "Name of the api gateway"
+}
+variable "gateway_authorizer" {
+  description = "Name of api gateway authoriser"
+}
+variable "logs_retention_days" {
+  description = "Number of days to retain logs"
+}
+variable "cognito_pool_name" {
+  description = "Name of cognito user pool"
+}
