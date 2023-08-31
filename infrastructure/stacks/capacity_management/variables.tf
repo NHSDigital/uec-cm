f344c720a0a6635bdfc24779dbbdcd9ba5242a8a
Tim Rickwood <tim.rickwood@nhs.net>
2023-08-31 11:20:03 +0100

    DR-353 Consistent use plurals

--- infrastructure/stacks/capacity_management/variables.tf
+++ infrastructure/stacks/capacity_management/variables.tf
@@ -11,7 +11,23 @@ variable "cognito_pool_name" {
   description = "Name of cognito user pool"
 }
 variable "healthcare_service_function_name" {
-  description = "Name of lambda function for healthcare service data manager"
+  description = "Name of lambda function for healthcare-services data manager"
+}
+variable "organisation_function_name" {
+  description = "Name of lambda function for organisations data manager"
+}
+variable "organisation_affiliation_function_name" {
+  description = "Name of lambda function for organisation-affiliations data manager"
+}
+variable "location_function_name" {
+  description = "Name of lambda function for locations data manager"
+}
+
+variable "questionnaire_function_name" {
+  description = "Name of lambda function for questionnaires data manager"
+}
+variable "questionnaire_response_function_name" {
+  description = "Name of lambda function for questionnaire-responses data manager"
 }
 variable "organisations_function_name" {
   description = "Name of lambda function for organisations data manager"
