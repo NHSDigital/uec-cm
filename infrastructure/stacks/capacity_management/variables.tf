6c84ab636e333419b69c6267b83bbeaa430cd924
Tim Rickwood <tim.rickwood@nhs.net>
2023-08-31 11:24:29 +0100

    DR-353 Update var decl

--- infrastructure/stacks/capacity_management/variables.tf
+++ infrastructure/stacks/capacity_management/variables.tf
@@ -10,23 +10,23 @@ variable "logs_retention_days" {
 variable "cognito_pool_name" {
   description = "Name of cognito user pool"
 }
-variable "healthcare_service_function_name" {
+variable "healthcare_services_function_name" {
   description = "Name of lambda function for healthcare-services data manager"
 }
-variable "organisation_function_name" {
+variable "organisations_function_name" {
   description = "Name of lambda function for organisations data manager"
 }
-variable "organisation_affiliation_function_name" {
+variable "organisation_affiliations_function_name" {
   description = "Name of lambda function for organisation-affiliations data manager"
 }
-variable "location_function_name" {
+variable "locations_function_name" {
   description = "Name of lambda function for locations data manager"
 }
 
-variable "questionnaire_function_name" {
+variable "questionnaires_function_name" {
   description = "Name of lambda function for questionnaires data manager"
 }
-variable "questionnaire_response_function_name" {
+variable "questionnaire_responses_function_name" {
   description = "Name of lambda function for questionnaire-responses data manager"
 }
 variable "organisations_function_name" {
