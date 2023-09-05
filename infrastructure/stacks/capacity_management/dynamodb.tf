b8dc9f0a66f3944e37e099793ab93aa13990b256
pira Maheswaran <piravinth.maheswaran1@nhs.net>
2023-09-05 15:47:17 +0100

    DR-386 Parameterising dynamodb names

--- infrastructure/stacks/capacity_management/dynamodb.tf
+++ infrastructure/stacks/capacity_management/dynamodb.tf
@@ -1,35 +1,35 @@
 module "dynamodb_organisations_table" {
   source = "../../modules/dynamodb"
 
-  table_name = "organisations"
+  table_name = var.organisations_dynamodb_table_name
 }
 
 module "dynamodb_organisation_affiliations_table" {
   source = "../../modules/dynamodb"
 
-  table_name = "organisation_affiliations"
+  table_name = var.organisation_affiliations_dynamodb_table_name
 }
 
 module "dynamodb_locations_table" {
   source = "../../modules/dynamodb"
 
-  table_name = "locations"
+  table_name = var.locations_dynamodb_table_name
 }
 
 module "dynamodb_healthcare_services_table" {
   source = "../../modules/dynamodb"
 
-  table_name = "healthcare_services"
+  table_name = var.healthcare_services_dynamodb_table_name
 }
 
 module "dynamodb_questionnaires_table" {
   source = "../../modules/dynamodb"
 
-  table_name = "questionnaires"
+  table_name = var.questionnaires_dynamodb_table_name
 }
 
 module "dynamodb_questionnaire_responses_table" {
   source = "../../modules/dynamodb"
 
-  table_name = "questionnaire_responses"
+  table_name = var.questionnaire_responses_dynamodb_table_name
 }
