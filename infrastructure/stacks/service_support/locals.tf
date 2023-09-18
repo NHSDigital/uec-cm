b76658818ae5fcdc0de68d2470e5c7d90305243b
Jonathan Pearce <j.pearce1@nhs.net>
2023-09-18 11:26:17 +0100

    DR-406 Added terraform for Athena

--- infrastructure/stacks/service_support/locals.tf
+++ infrastructure/stacks/service_support/locals.tf
@@ -6,9 +6,9 @@ locals {
 
   athena_bucket_name                  = "${var.project}-${var.environment}-${var.athena_bucket_name}"
   athena_database_name                = replace("${var.athena_database_name}", "-", "_")
-  athena_workgroup_name               = "${var.athena_workgroup_name}"
-  athena_data_catalogue_name          = "${var.athena_data_catalogue_name}"
-  athena_data_catalogue_lambda_name   = "${var.athena_data_catalogue_lambda_name}"
-  athena_dynamo_connect_cf_stack_name = "${var.athena_dynamo_connect_cf_stack_name}"
+  athena_workgroup_name               = var.athena_workgroup_name
+  athena_data_catalogue_name          = var.athena_data_catalogue_name
+  athena_data_catalogue_lambda_name   = var.athena_data_catalogue_lambda_name
+  athena_dynamo_connect_cf_stack_name = var.athena_dynamo_connect_cf_stack_name
 }
 
