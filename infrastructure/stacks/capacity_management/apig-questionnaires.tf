f166a214d9af8982264d681bc4ae5258bde83fb5
Tim Rickwood <tim.rickwood@nhs.net>
2023-09-22 15:13:42 +0100

    DR-412 Prep for review

--- infrastructure/stacks/capacity_management/apig-questionnaires.tf
+++ infrastructure/stacks/capacity_management/apig-questionnaires.tf
@@ -1,140 +1,135 @@
-#######
-# Organisations endpoint
-#######
-# TODO
-# Uncomment authorizer and switch authorization to COGNITO_USER_POOLS
+
 resource "aws_api_gateway_resource" "questionnaires_resource" {
-  parent_id   = aws_api_gateway_rest_api.cm_rest_api.root_resource_id
+  parent_id   = module.cm_rest_api.root_resource_id
   path_part   = "questionnaires"
-  rest_api_id = aws_api_gateway_rest_api.cm_rest_api.id
+  rest_api_id = module.cm_rest_api.rest_api_id
 }
 
-#######
-# REST methods attached to the endpoint - POST PUT GET DELETE
-# links method to apig, resource/endpoint and authorizer
-#######
-resource "aws_api_gateway_method" "questionnaires_POST" {
-  authorization = "NONE"
-  http_method   = "POST"
-  resource_id   = aws_api_gateway_resource.questionnaires_resource.id
-  rest_api_id   = aws_api_gateway_rest_api.cm_rest_api.id
-
-  depends_on = [
-    aws_api_gateway_resource.questionnaires_resource
-  ]
-}
-
-resource "aws_api_gateway_method" "questionnaires_PUT" {
-  authorization = "NONE"
-  http_method   = "PUT"
-  resource_id   = aws_api_gateway_resource.questionnaires_resource.id
-  rest_api_id   = aws_api_gateway_rest_api.cm_rest_api.id
-
-  depends_on = [
-    aws_api_gateway_resource.questionnaires_resource
-  ]
-}
-
-resource "aws_api_gateway_method" "questionnaires_GET" {
+resource "aws_api_gateway_method" "questionnaires_get" {
   authorization = "NONE"
   http_method   = "GET"
   resource_id   = aws_api_gateway_resource.questionnaires_resource.id
-  rest_api_id   = aws_api_gateway_rest_api.cm_rest_api.id
+  rest_api_id   = module.cm_rest_api.rest_api_id
 
   depends_on = [
     aws_api_gateway_resource.questionnaires_resource
   ]
 }
 
-resource "aws_api_gateway_method" "questionnaires_DELETE" {
+module "questionnaires_integrations_get" {
+  source               = "../../modules/api-gateway-integrations"
+  aws_region           = var.aws_region
+  account_id           = local.account_id
+  rest_api_id          = module.cm_rest_api.rest_api_id
+  http_method          = aws_api_gateway_method.questionnaires_get.http_method
+  lambda_function_name = var.questionnaires_function_name
+  gateway_resource_id  = aws_api_gateway_resource.questionnaires_resource.id
+
+  depends_on = [
+    aws_api_gateway_resource.questionnaires_resource,
+    aws_api_gateway_method.questionnaires_get
+  ]
+}
+
+resource "aws_api_gateway_method" "questionnaires_put" {
+  authorization = "NONE"
+  http_method   = "PUT"
+  resource_id   = aws_api_gateway_resource.questionnaires_resource.id
+  rest_api_id   = module.cm_rest_api.rest_api_id
+
+  depends_on = [
+    aws_api_gateway_resource.questionnaires_resource
+  ]
+}
+module "questionnaires_integrations_put" {
+  source               = "../../modules/api-gateway-integrations"
+  aws_region           = var.aws_region
+  account_id           = local.account_id
+  rest_api_id          = module.cm_rest_api.rest_api_id
+  http_method          = aws_api_gateway_method.questionnaires_put.http_method
+  lambda_function_name = var.questionnaires_function_name
+  gateway_resource_id  = aws_api_gateway_resource.questionnaires_resource.id
+
+  depends_on = [
+    aws_api_gateway_resource.questionnaires_resource,
+    aws_api_gateway_method.questionnaires_put
+  ]
+}
+
+resource "aws_api_gateway_method" "questionnaires_post" {
+  authorization = "NONE"
+  http_method   = "POST"
+  resource_id   = aws_api_gateway_resource.questionnaires_resource.id
+  rest_api_id   = module.cm_rest_api.rest_api_id
+
+  depends_on = [
+    aws_api_gateway_resource.questionnaires_resource
+  ]
+}
+
+module "questionnaires_integrations_post" {
+  source               = "../../modules/api-gateway-integrations"
+  aws_region           = var.aws_region
+  account_id           = local.account_id
+  rest_api_id          = module.cm_rest_api.rest_api_id
+  http_method          = aws_api_gateway_method.questionnaires_post.http_method
+  lambda_function_name = var.questionnaires_function_name
+  gateway_resource_id  = aws_api_gateway_resource.questionnaires_resource.id
+
+  depends_on = [
+    aws_api_gateway_resource.questionnaires_resource,
+    aws_api_gateway_method.questionnaires_post
+  ]
+}
+
+resource "aws_api_gateway_method" "questionnaires_delete" {
   authorization = "NONE"
   http_method   = "DELETE"
   resource_id   = aws_api_gateway_resource.questionnaires_resource.id
-  rest_api_id   = aws_api_gateway_rest_api.cm_rest_api.id
+  rest_api_id   = module.cm_rest_api.rest_api_id
 
   depends_on = [
     aws_api_gateway_resource.questionnaires_resource
   ]
 }
 
-#####
-# Link lambda to HTTP method previously attached to api resource
-# Note integration_http_method - Lambda functions can only be invoked via POST.
-#####
+module "questionnaires_integrations_delete" {
+  source               = "../../modules/api-gateway-integrations"
+  aws_region           = var.aws_region
+  account_id           = local.account_id
+  rest_api_id          = module.cm_rest_api.rest_api_id
+  http_method          = aws_api_gateway_method.questionnaires_delete.http_method
+  lambda_function_name = var.questionnaires_function_name
+  gateway_resource_id  = aws_api_gateway_resource.questionnaires_resource.id
 
-resource "aws_api_gateway_integration" "questionnaires_GET_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.cm_rest_api.id
-  resource_id             = aws_api_gateway_resource.questionnaires_resource.id
-  http_method             = aws_api_gateway_method.questionnaires_GET.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/arn:aws:lambda:${var.aws_region}:${data.aws_caller_identity.current.account_id}:function:${var.questionnaires_function_name}${local.workspace_suffix}/invocations"
   depends_on = [
     aws_api_gateway_resource.questionnaires_resource,
-    aws_api_gateway_method.questionnaires_GET
+    aws_api_gateway_method.questionnaires_delete
   ]
 }
 
-resource "aws_api_gateway_integration" "questionnaires_POST_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.cm_rest_api.id
-  resource_id             = aws_api_gateway_resource.questionnaires_resource.id
-  http_method             = aws_api_gateway_method.questionnaires_POST.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/arn:aws:lambda:${var.aws_region}:${data.aws_caller_identity.current.account_id}:function:${var.questionnaires_function_name}${local.workspace_suffix}/invocations"
-  depends_on = [
-    aws_api_gateway_resource.questionnaires_resource,
-    aws_api_gateway_method.questionnaires_POST
-  ]
+######
+# Lambda permissions
+######
+module "questionnaires_aws_lambda_permission" {
+  source               = "../../modules/api-gateway-permissions"
+  aws_region           = var.aws_region
+  account_id           = local.account_id
+  lambda_function_name = var.questionnaires_function_name
+  rest_api_id          = module.cm_rest_api.rest_api_id
 }
 
-resource "aws_api_gateway_integration" "questionnaires_PUT_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.cm_rest_api.id
-  resource_id             = aws_api_gateway_resource.questionnaires_resource.id
-  http_method             = aws_api_gateway_method.questionnaires_PUT.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/arn:aws:lambda:${var.aws_region}:${data.aws_caller_identity.current.account_id}:function:${var.questionnaires_function_name}${local.workspace_suffix}/invocations"
-  depends_on = [
-    aws_api_gateway_resource.questionnaires_resource,
-    aws_api_gateway_method.questionnaires_PUT
-  ]
-}
-
-resource "aws_api_gateway_integration" "questionnaires_DELETE_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.cm_rest_api.id
-  resource_id             = aws_api_gateway_resource.questionnaires_resource.id
-  http_method             = aws_api_gateway_method.questionnaires_DELETE.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/arn:aws:lambda:${var.aws_region}:${data.aws_caller_identity.current.account_id}:function:${var.questionnaires_function_name}${local.workspace_suffix}/invocations"
-  depends_on = [
-    aws_api_gateway_resource.questionnaires_resource,
-    aws_api_gateway_method.questionnaires_DELETE
-  ]
-}
-
-#####
-# Permission for apig to call lambda
-#####
-resource "aws_lambda_permission" "questionnaires_lambda_permission" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = "${var.questionnaires_function_name}${local.workspace_suffix}"
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:${var.aws_region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.cm_rest_api.id}/*/*"
-}
 #####
 # CORS
 #####
-module "enable_cors_on_questionnaires" {
+module "enable_cors_on_questionnaires_resource_services" {
   source          = "squidfunk/api-gateway-enable-cors/aws"
   version         = "0.3.3"
-  api_id          = aws_api_gateway_rest_api.cm_rest_api.id
+  api_id          = module.cm_rest_api.rest_api_id
   api_resource_id = aws_api_gateway_resource.questionnaires_resource.id
 
   depends_on = [
     aws_api_gateway_resource.questionnaires_resource,
-    aws_api_gateway_rest_api.cm_rest_api
+    module.cm_rest_api
   ]
 }
--- infrastructure/stacks/temp/apig.tf
+++
@@ -1,5 +0,0 @@
-module "cm_rest_api" {
-  source = "../../modules/api-gateway-rest-api"
-
-  rest_api_name = var.rest_api_name
-}
