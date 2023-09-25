e57214d41bd5073e8cd73bc58923d4a03451e3d8
Tim Rickwood <tim.rickwood@nhs.net>
2023-09-25 12:08:46 +0100

    DR-412 Pre review tidy

--- infrastructure/modules/api-gateway-rest-api/outputs.tf
+++ infrastructure/modules/api-gateway-rest-api/outputs.tf
@@ -1,5 +1,5 @@
 output "rest_api_id" {
-  description = "The name of the bucket."
+  description = "The unique id of the api gateway rest api."
   value       = resource.aws_api_gateway_rest_api.rest_api.id
 }
 output "root_resource_id" {
