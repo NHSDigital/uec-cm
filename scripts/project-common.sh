963a3704566097fd0ae13d27d57cdce2e39b5350
Tim Rickwood <tim.rickwood@nhs.net>
2023-10-16 09:36:22 +0100

    DR-354 Fix broken bootstrap (#32)
    <!-- markdownlint-disable-next-line first-line-heading -->
    ## Description
    Previous code changed the name of the script that actions terraform
    (plan, apply etc). The name change was not included in the bootstrapper
    workflow and that broke the pipeline in main
    <!-- Describe your changes in detail. -->
    
    ## Context
    
    <!-- Why is this change required? What problem does it solve? -->
    
    ## Type of changes
    
    <!-- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply. -->
    
    - [ ] Refactoring (non-breaking change)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would change existing
    functionality)
    - [x] Bug fix (non-breaking change which fixes an issue)
    
    ## Checklist
    
    <!-- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    
    - [x] I am familiar with the [contributing
    guidelines](../docs/CONTRIBUTING.md)
    - [x] I have followed the code style of the project
    - [ ] I have added tests to cover my changes
    - [ ] I have updated the documentation accordingly
    - [ ] This PR is a result of pair or mob programming
    
    ---
    
    ## Sensitive Information Declaration
    
    To ensure the utmost confidentiality and protect your and others
    privacy, we kindly ask you to NOT including [PII (Personal Identifiable
    Information) / PID (Personal Identifiable
    Data)](https://digital.nhs.uk/data-and-information/keeping-data-safe-and-benefitting-the-public)
    or any other sensitive data in this PR (Pull Request) and the codebase
    changes. We will remove any PR that do contain any sensitive
    information. We really appreciate your cooperation in this matter.
    
    - [x] I confirm that neither PII/PID nor sensitive data are included in
    this PR and the codebase changes.
    
    ---------
    
    Co-authored-by: Jonathan Pearce <j.pearce1@nhs.net>

--- scripts/project-common.sh
+++ scripts/project-common.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-# Used by bootstrapper and infra-deploy scripts so defined once here
+# Used by bootstrapper and app-build-deploy scripts so defined once here
 export TF_VAR_repo_name=uec-cm
 export ACCOUNT_PROJECT=cm
