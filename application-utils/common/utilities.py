142cc0d1cef29eee58520696995453c5a8a5875a
Tim Rickwood <tim.rickwood@nhs.net>
2023-11-29 11:22:16 +0000

    DR-888 set up sonarcloud (#44)
    <!-- markdownlint-disable-next-line first-line-heading -->
    ## Description
    Set up sonarcloud static analysis
    <!-- Describe your changes in detail. -->
    
    ## Context
    
    <!-- Why is this change required? What problem does it solve? -->
    
    ## Type of changes
    
    <!-- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply. -->
    
    - [ ] Refactoring (non-breaking change)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would change existing
    functionality)
    - [ ] Bug fix (non-breaking change which fixes an issue)
    
    ## Checklist
    
    <!-- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    
    - [ ] I am familiar with the [contributing
    guidelines](../docs/CONTRIBUTING.md)
    - [ ] I have followed the code style of the project
    - [ ] I have added tests to cover my changes
    - [ ] I have updated the documentation accordingly
    - [ ] This PR is a result of pair or mob programming
    
    ---
    
    ## Sensitive Information Declaration
    
    To ensure the utmost confidentiality and protect your and others
    privacy, we kindly ask you to NOT including [PII (Personal Identifiable
    Information) / PID (Personal Identifiable
    Data)](https://digital.nhs.uk/data-and-information/keeping-data-safe-and-benefitting-the-public)
    or any other sensitive data in this PR (Pull Request) and the codebase
    changes. We will remove any PR that do contain any sensitive
    information. We really appreciate your cooperation in this matter.
    
    - [ ] I confirm that neither PII/PID nor sensitive data are included in
    this PR and the codebase changes.

--- application-utils/common/utilities.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def get_table_name(table_name):
-    workspace_table_name = table_name
-    if os.getenv("WORKSPACE") is not None and os.getenv("WORKSPACE") != "":
-        workspace_table_name = table_name + "-" + os.getenv("WORKSPACE")
-    return workspace_table_name
