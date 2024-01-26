29a2207ab9f92d6a2e38a4aa49a47237a4273d53
timaNHS <timi.abel1@nhs.net>
2024-01-26 11:27:45 +0000

    DR-667 Updated the force_destroy flag to true

--- infrastructure/modules/s3/main.tf
+++ infrastructure/modules/s3/main.tf
@@ -6,7 +6,7 @@ module "s3" {
   attach_policy  = var.attach_policy
   policy         = var.policy
   lifecycle_rule = var.lifecycle_rule_inputs
-  force_destroy  = false
+  force_destroy  = true
 
   block_public_acls       = var.block_public_acls
   block_public_policy     = var.block_public_policy
