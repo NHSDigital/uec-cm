475664a4b2d787ebfeb40350929ad7c2de3d587d
asudbury-nhs <adrian.sudbury1@nhs.net>
2024-02-08 15:30:19 +0000

    DR-716 Fixed formatting issue

--- src/frontend/src/pages/organisations/add.tsx
+++ src/frontend/src/pages/organisations/add.tsx
@@ -4,9 +4,9 @@ import { Label } from 'nhsuk-react-components';
 const AddOrganisationPage: React.FC = () => {
     return (
         <>
-         <div className="nhsuk-width-container">
-            <Label isPageHeading={true} size='m'>Placeholder for adding an organisation</Label>
-         </div>
+            <div className="nhsuk-width-container">
+                <Label isPageHeading={true} size='m'>Placeholder for adding an organisation</Label>
+            </div>
         </>
     );
 };
