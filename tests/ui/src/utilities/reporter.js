aae15139bdeb4b0ec89757d08dc58da473492b36
solomon209 <155565138+solomon209@users.noreply.github.com>
2024-02-15 13:31:49 +0000

    Task/dr 715 allure reporter integration (#73)
    * DR-715 Integrate allure reporter
    
    * DR-715 Integrate allure reporter

---
+++ tests/ui/src/utilities/reporter.js
@@ -0,0 +1,42 @@
+const { CucumberJSAllureFormatter, AllureRuntime } = require("allure-cucumberjs");
+
+class Reporter extends CucumberJSAllureFormatter {
+  constructor(options) {
+    super(
+      options,
+      new AllureRuntime({
+        resultsDir: "./allure-results"
+      }),
+      {
+        labels: [
+          {
+            pattern: [/@epic:(.*)/],
+            name: "epic",
+          },
+          {
+            pattern: [/@story:(.*)/],
+            name: "story",
+          },
+          {
+            pattern: [/@severity:(.*)/],
+            name: "severity",
+          },
+        ],
+        links: [
+          {
+            pattern: [/@bug=(.*)/],
+            type: "issue",
+            urlTemplate: "https://nhsd-jira.digital.nhs.uk/browse/issue/%s",
+          },
+          {
+            pattern: [/@tms=(.*)/],
+            type: "tms",
+            urlTemplate: "https://nhsd-jira.digital.nhs.uk/browse/tms/%s",
+          },
+        ],
+      },
+    );
+  }
+}
+
+module.exports = Reporter;
