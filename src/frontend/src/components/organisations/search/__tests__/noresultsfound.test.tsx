dc8f3a556e01e786edae15a4084b0a66babb26c9
asudbury-nhs <153083471+asudbury-nhs@users.noreply.github.com>
2024-02-20 12:29:17 +0000

    Task/dr 719 display no results (#77)
    * DR-719 Display no results
    
    * DR-719 Display no results
    
    * DR-719 Fixed error when invalid search criteria
    
    * DR-719 Fix formatting issue
    
    * DR-719 Used beforeeach in tests to reduce duplicated code

---
+++ src/frontend/src/components/organisations/search/__tests__/noresultsfound.test.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { act, render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
+import NoResultsFound from '../noresultsfound';
+
+beforeEach(() => {
+  render(<NoResultsFound />);
+});
+
+describe('NoResultsFound', () => {
+
+  it('should display the correct page heading', () => {
+    const heading = screen.getByRole('heading', { level: 1, name: /no results found/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('should display the question about adding a new organization', () => {
+    const question = screen.getByText(/would you like to add a new organisation/i);
+    expect(question).toBeInTheDocument();
+  });
+
+  it('should update the selected option when an input is changed to yes', () => {
+    const radioYes = screen.getByTestId('yes-radio') as HTMLOptionElement;
+    const radioNo = screen.getByTestId('no-radio') as HTMLOptionElement;
+
+    act(() => {
+      userEvent.click(radioYes);
+    });
+
+    expect(radioYes).toBeChecked();
+    expect(radioNo).not.toBeChecked();
+  });
+
+  it('should update the selected option when an input is changed to no', () => {
+    const radioYes = screen.getByTestId('yes-radio') as HTMLOptionElement;
+    const radioNo = screen.getByTestId('no-radio') as HTMLOptionElement;
+
+    act(() => {
+      userEvent.click(radioNo);
+    });
+
+    expect(radioYes).not.toBeChecked();
+    expect(radioNo).toBeChecked();
+  });
+});
