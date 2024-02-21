afd510ba93857cc6e2b6f4b4ea3a56c3b31a3dac
timaNHS <timi.abel1@nhs.net>
2024-02-21 10:27:10 +0000

    DR-667 Added axe accessibility tests updated accessibility file

--- tests/ui/src/utilities/acccessibility.ts
+++ tests/ui/src/utilities/acccessibility.ts
@@ -1,7 +1,7 @@
-import { Page } from 'playwright';
-import {pageFixture} from "../../src/hooks/pageFixture";
-import { AxeBuilder } from '@axe-core/playwright';
-import { createHtmlReport }  from 'axe-html-reporter';
+import { Page } from "playwright";
+import { pageFixture } from "../hooks/pageFixture";
+import { AxeBuilder } from "@axe-core/playwright";
+import { createHtmlReport } from "axe-html-reporter";
 
 export default class Accessibility {
 
@@ -9,21 +9,18 @@ export default class Accessibility {
     pageFixture.page = page;
   }
 
-  async runAxeCheck()
+  async runAxeCheck() {
+    const accessibilityScanResults = await new AxeBuilder({
+      page: pageFixture.page,
+    }).analyze();
 
-  {const accessibilityScanResults = await new AxeBuilder({ page: pageFixture.page }
-  ).analyze();
+    const reportHTML = createHtmlReport({
+      results: accessibilityScanResults,
+      options: {
+        projectKey: "uec-cm",
+      },
+    });
 
-  const reportHTML = createHtmlReport({
-    results: accessibilityScanResults,
-    options: {
-      projectKey: "uec-cm"
-    },
-  });
-
-  if (accessibilityScanResults.violations.length > 0) {
-    throw new Error(`Accessibility violations found: ${JSON.stringify(accessibilityScanResults.violations)}`);
-  }
-
+    return accessibilityScanResults;
   }
 }
