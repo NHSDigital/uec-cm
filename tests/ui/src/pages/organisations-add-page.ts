487299cfaaab219bebb28531a55549426aabf251
solomon209 <155565138+solomon209@users.noreply.github.com>
2024-02-27 16:10:26 +0000

    Task/dr 743 refactor uitest framework (#86)
    * DR-743 Refactor uitest framework
    
    * DR-743 Change file names
    
    * DR-743 Fix pr comment

---
+++ tests/ui/src/pages/organisations-add-page.ts
@@ -0,0 +1,68 @@
+
+import { expect, Locator, Page } from "@playwright/test";
+import { pageFixture } from "../../src/hooks/pageFixture";
+
+export default class OrganisationsPage {
+  constructor(page: Page) {
+    pageFixture.page = page;
+  }
+
+  static readonly add = 'add-card-link'
+  static readonly searchInstructions = 'search-by'
+  static readonly fieldError = (field: string) => `#${field}-error-message`
+  static readonly orgAddSearchBtn = 'search-button'
+  static readonly orgAddNewOption = 'yes-radio'
+  static readonly orgErrorSummaryLink = (type: string) => `error-summary-${type}-link`
+  static readonly orgErrorSummary = (type: string) => `error-summary-${type}`
+  static readonly inputField = (field: string) => `${field}-input`
+
+  // Getters
+  async getOrgErrorSummaryLink(type: string): Promise<Locator> {
+    return pageFixture.page.getByTestId(OrganisationsPage.orgErrorSummaryLink(type));
+  }
+
+  async getOrgErrorSummary(type: string): Promise<Locator> {
+    return pageFixture.page.getByTestId(OrganisationsPage.orgErrorSummary(type));
+  }
+
+  async getOrgInputField(type: string): Promise<Locator> {
+    return pageFixture.page.getByTestId(OrganisationsPage.inputField(type));
+  }
+
+  async getFieldErrorMessage(field: string): Promise<Locator> {
+    return pageFixture.page.getByTestId(OrganisationsPage.fieldError(field));
+  }
+
+  async getFieldError(field: string): Promise<Locator> {
+    return pageFixture.page.locator(OrganisationsPage.fieldError(field));
+  }
+
+  async getSearchInstructions(): Promise<Locator> {
+    return pageFixture.page.getByTestId(OrganisationsPage.searchInstructions);
+  }
+
+  // Methods
+  async clickAdd() {
+    await pageFixture.page.getByTestId(OrganisationsPage.add).click();
+  }
+
+  async clickOrgAddSearch() {
+    await pageFixture.page.getByTestId(OrganisationsPage.orgAddSearchBtn).click();
+  }
+
+  async inputTextInField(field: string, text: string) {
+    await pageFixture.page.getByTestId(OrganisationsPage.inputField(field)).fill(text);
+  }
+
+  async errorMessage(text: string): Promise<Locator> {
+    return pageFixture.page.getByText(text);
+  }
+
+  async addOrganisationOptionIsSelected(): Promise<Locator> {
+    return pageFixture.page.getByTestId(OrganisationsPage.orgAddNewOption);
+  }
+
+  async clickOrgInputField(type: string) {
+    (await this.getOrgInputField(type)).click();
+  }
+}
---
+++ tests/ui/src/pages/search-page.ts
@@ -0,0 +1,32 @@
+import { Locator, Page } from "@playwright/test";
+import { pageFixture } from "../../src/hooks/pageFixture";
+import Accessibility from "../utilities/accessibility"
+import countFilesInDirectory from "../utilities/fileCount"
+
+export default class SearchPage extends Accessibility {
+  constructor(page: Page) {
+    super(page);
+    pageFixture.page = page;
+  }
+
+  async headerIsReturned(searchResultsHeader: string) {
+    return pageFixture.page.getByRole('heading', { name: searchResultsHeader });
+  }
+
+  async textIsReturned(searchResultsText: string): Promise<Locator> {
+    return pageFixture.page.getByText(searchResultsText, { exact: true });
+  }
+
+  async runAxeCheck(testId: string) {
+    return super.runAxeCheck(testId);
+  }
+
+  async expectAccessibilityCheckFails(partFileName: string) {
+    const fileCount = await countFilesInDirectory("./accessibility-reports/artifacts",partFileName);
+    return fileCount
+  }
+
+  async linkIsReturned(searchResultsLink: string): Promise<Locator> {
+    return pageFixture.page.getByRole("link" , { name: searchResultsLink } );
+  }
+}
