a06277db0b23eb1f27d98f843da397a5a26ba605
Tamara Goldschmidt <tamara.goldschmidt@nhs.net>
2024-03-05 14:53:48 +0000

    DR-989 Adding in axe library

--- tests/uiv2/playwright.config.ts
+++ tests/uiv2/playwright.config.ts
@@ -32,7 +32,8 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: '',
-
+    //Capture screenshot after each test failure
+    screenshot: 'only-on-failure',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
