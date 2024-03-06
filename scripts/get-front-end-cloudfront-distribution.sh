674e737d0b477592394b1acc0e2ab90d2eb5da3f
asudbury-nhs <adrian.sudbury1@nhs.net>
2024-03-06 14:31:42 +0000

    DR-751 Export the distribution id v6

--- scripts/get-front-end-cloudfront-distribution.sh
+++ scripts/get-front-end-cloudfront-distribution.sh
@@ -3,22 +3,18 @@
 # This script gets the cloud front distribution id for the front end
 #
 
-echo "ENVIRONMENT: $ENVIRONMENT"
-echo "WORKSPACE: $WORKSPACE"
-echo "AWS_REGION: $AWS_REGION"
-
 if [ -z "$ENVIRONMENT" ]; then
-  echo "Set the ENVIRONMENT environment variable"
+  echo "Set the ENVIRONMENT environment variable" >&2
   exit 1;
 fi
 
 if [ -z "$WORKSPACE" ]; then
-  echo "Set the WORKSPACE environment variable"
+  echo "Set the WORKSPACE environment variable" >&2
   exit 1;
 fi
 
 if [ -z "$AWS_REGION" ]; then
-  echo "Set the AWS_REGION environment variable"
+  echo "Set the AWS_REGION environment variable" >&2
   exit 1;
 fi
 
@@ -28,10 +24,7 @@ else
   FRONT_END_DOMAIN="nhse-uec-cm-${ENVIRONMENT}-front-end-${WORKSPACE}.s3.${AWS_REGION}.amazonaws.com"
 fi
 
-echo "FRONT_END_DOMAIN: $FRONT_END_DOMAIN"
-
 QUERY_COMMAND="DistributionList.Items[?Origins.Items[0].DomainName=='$FRONT_END_DOMAIN'].{Id: Id}[0].Id"
 
 DISTRIBUTION_ID=$(aws cloudfront list-distributions --query "$QUERY_COMMAND")
-echo "DISTRIBUTION_ID: $DISTRIBUTION_ID"
-export DISTRIBUTION_ID=$DISTRIBUTION_ID
+echo $DISTRIBUTION_ID
