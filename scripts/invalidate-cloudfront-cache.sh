e43f04aff81c2b24d81bdde0b266a725a956f598
asudbury-nhs <adrian.sudbury1@nhs.net>
2024-03-06 16:00:43 +0000

    DR-751 Remove debug code

--- scripts/invalidate-cloudfront-cache.sh
+++ scripts/invalidate-cloudfront-cache.sh
@@ -10,9 +10,7 @@ if [ -z "$DISTRIBUTION_ID" ]; then
 else
   echo "Invalidating CloudFront cache"
 
-  # Remove double quotes from DISTRIBUTION_ID if they exist
   DISTRIBUTION_ID=$(echo "$DISTRIBUTION_ID" | sed 's/"//g')
-
-  aws cloudfront get-distribution --id $DISTRIBUTION_ID --debug
-  aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*" --debug
+  
+  aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*"
 fi
