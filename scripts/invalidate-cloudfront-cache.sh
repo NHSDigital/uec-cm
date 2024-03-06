0baf936d50df5aeaa48d232deb015313a6b085a9
asudbury-nhs <adrian.sudbury1@nhs.net>
2024-03-06 12:16:43 +0000

    DR-751 Add invalidate cloudfront distribution step

--- scripts/invalidate-cloudfront-cache.sh
+++ scripts/invalidate-cloudfront-cache.sh
@@ -7,6 +7,8 @@ echo "DISTRIBUTION_ID: $DISTRIBUTION_ID"
 
 if [ -z "$DISTRIBUTION_ID" ]; then
   echo "DISTRIBUTION_ID is not set."
+else
+  echo "Invalidating cloudfront cache"
+  aws cloudfront create-invalidation --distribution-id "$DISTRIBUTION_ID" --paths /*
 fi
 
-aws cloudfront create-invalidation --distribution-id "$DISTRIBUTION_ID" --paths /*
