f3213bc5c9d66d4681c2ccd297536a6344a15661
asudbury-nhs <adrian.sudbury1@nhs.net>
2024-03-07 13:48:02 +0000

    DR-768 Create rightchevron icon

---
+++ src/frontend/src/components/images/rightchevron.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const RightChevronIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" aria-hidden="true" focusable="false">
+    <circle cx="13.333" cy="13.333" r="13.333" fill="#005eb8" />
+    <g fill="none" stroke="#fff" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="2.667">
+      <path d="M15.438 13l-3.771 3.771" />
+      <path data-name="Path" d="M11.667 9.229L15.438 13" />
+    </g>
+  </svg>
+  );
+};
+
+export default RightChevronIcon;
