422b38a2a158ce44eba02a43e3afd9e5a94c177e
Tamara Goldschmidt <tamara.goldschmidt@nhs.net>
2024-05-10 18:30:35 +0100

    DR-856 Added in variable

--- infrastructure/stacks/test/variables.tf
+++ infrastructure/stacks/test/variables.tf
@@ -1,3 +1,8 @@
 variable "s3_bucket_name" {
   description = "Name of s3 bucket that holds downloaded csv"
 }
+variable "force_destroy" {
+  description = "Whether to forcefully destroy the bucket when it contains objects"
+  type        = bool
+  default     = true
+}
