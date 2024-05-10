076dc26c02428f2668f65bfe88ebe6162541fab3
Tamara Goldschmidt <tamara.goldschmidt@nhs.net>
2024-05-10 17:32:05 +0100

    DR-856 Removing a extra hyphen

--- infrastructure/stacks/test/s3_bucket.tf
+++ infrastructure/stacks/test/s3_bucket.tf
@@ -1,4 +1,4 @@
 module "s3_bucket" {
   source      = "../../modules/s3"
-  bucket_name = "${var.s3_bucket_name}-${var.environment}-${local.workspace_suffix}"
+  bucket_name = "${var.s3_bucket_name}-${var.environment}${local.workspace_suffix}"
 }
