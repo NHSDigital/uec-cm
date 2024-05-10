0d9e9f16ea404dc7596686c0d17f43dcfe7ac7c2
Tamara Goldschmidt <tamara.goldschmidt@nhs.net>
2024-05-10 18:15:19 +0100

    DR-856 Use the force delete var

--- infrastructure/stacks/test/s3_bucket.tf
+++ infrastructure/stacks/test/s3_bucket.tf
@@ -1,4 +1,5 @@
 module "s3_bucket" {
-  source      = "../../modules/s3"
-  bucket_name = "${var.s3_bucket_name}-${var.environment}${local.workspace_suffix}"
+  source        = "../../modules/s3"
+  bucket_name   = "${var.s3_bucket_name}-${var.environment}${local.workspace_suffix}"
+  force_destroy = var.force_destroy
 }
