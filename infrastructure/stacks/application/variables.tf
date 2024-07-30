5db88b056cfc348b62921798b875e117a0342b7f
Tim Rickwood <tim.rickwood@nhs.net>
2024-07-30 09:54:32 +0100

    DR-1052 Wip commit scope and region issue

--- infrastructure/stacks/application/variables.tf
+++ infrastructure/stacks/application/variables.tf
@@ -12,28 +12,3 @@ variable "force_destroy" {
   default     = true
 }
 
-# Waf related
-variable "waf_dashboard_name" {
-  description = "Name of cloudwatch dashboard for wafv2"
-}
-
-variable "waf_name" {
-  description = "Name of the WAF ACL"
-}
-
-variable "waf_log_group_name" {
-  description = "Name of the cloudwatch log group generated for WAF ACL"
-}
-
-variable "non_gb_rule_metric_name" {
-  description = "Name of metric for the non gp geo rule"
-}
-
-variable "ip_reputation_list_metric_name" {
-  description = "Name of metric for ip reputation list rule"
-}
-
-variable "generate_waf_logs" {
-  description = "Boolean value to determine whether or not we generate WAF logging"
-}
-
