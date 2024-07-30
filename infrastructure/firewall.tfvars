4b74699ad8627894f80ff5950438f83da1ddc767
Tim Rickwood <tim.rickwood@nhs.net>
2024-07-30 11:05:08 +0100

    DR-1052 Specify region and provider for us

--- infrastructure/firewall.tfvars
+++ infrastructure/firewall.tfvars
@@ -4,3 +4,4 @@ waf_log_group_name             = "aws-waf-logs"
 non_gb_rule_metric_name        = "waf-non-GB-geo-match-metric"
 ip_reputation_list_metric_name = "waf-aws-ip-reputation-list-metric"
 generate_waf_logs              = false
+aws_region                     = "us-east-1"
--- infrastructure/stacks/application/data.tf
+++ infrastructure/stacks/application/data.tf
@@ -1,6 +1,25 @@
 # Look up waf
+provider "us-east-1" {
+  alias                       = "us1"
+  region                      = "us-east-1"
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = false
+
+}
+provider "eu-west-2" {
+  alias                       = "eu"
+  region                      = "eu-west-2"
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = false
+
+}
 data "aws_wafv2_web_acl" "waf_acl" {
-  name  = "${var.project}-waf-acl"
-  scope = "CLOUDFRONT"
+  name     = "${var.project}-waf-acl"
+  scope    = "CLOUDFRONT"
+  provider = eu-west-2.eu
 }
 
