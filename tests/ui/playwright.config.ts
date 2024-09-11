6d4227f734c25a3ffd629d1eb571afc20de61e40
csimpson24334 <chloe.simpson24@nhs.net>
2024-09-11 15:05:03 +0100

    DR-1109 Updated read csv test

--- tests/ui/playwright.config.ts
+++ tests/ui/playwright.config.ts
@@ -30,24 +30,24 @@ export default defineConfig({
   },
 
   projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
+  {
+    name: 'chromium',
+    use: { ...devices['Desktop Chrome'] },
+  },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+  {
+    name: 'firefox',
+    use: { ...devices['Desktop Firefox'] },
+  },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+  {
+    name: 'webkit',
+    use: { ...devices['Desktop Safari'] },
+  },
 
-  //   /* Test against branded browsers. */
-  //   {
-  //     name: 'Microsoft Edge',
+  /* Test against branded browsers. */
+  //{
+  // name: 'Microsoft Edge',
   //     use: { ...devices['Desktop Edge'], channel: 'msedge' },
   //   },
   //   {
