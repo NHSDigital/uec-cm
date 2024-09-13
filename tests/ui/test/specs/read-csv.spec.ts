91a2630b58d9562a214b98d5f0daf2f5946450fd
Tamara Goldschmidt <tamara.goldschmidt@nhs.net>
2024-09-13 12:54:34 +0100

    DR-1109 Removing some awaits and async

--- tests/ui/test/specs/read-csv.spec.ts
+++ tests/ui/test/specs/read-csv.spec.ts
@@ -10,27 +10,27 @@ test.describe('As a user I want to be able to read downloaded csv files', {
 }, async () => {
 
   test.beforeEach(async ({ page }, testInfo) => {
-    await allure.parentSuite(testInfo.project.name);
-    await allure.suite("Tests downloaded files");
-    await allure.subSuite("Tests csv files");
+    allure.parentSuite(testInfo.project.name);
+    allure.suite("Tests downloaded files");
+    allure.subSuite("Tests csv files");
     await test.step('Navigate to landing page', async () => {
       await page.goto('/');
       filePath = path.join(__dirname, `../../downloads/data.csv`);
     });
   });
 
-  test('The data.csv file exists', async () => {
+  test('The data.csv file exists',  () => {
     expect(isFileExists(filePath)).toBeTruthy;
   });
 
-  test('The data.csv file has correct headers', async () => {
+  test('The data.csv file has correct headers',  () => {
       expect(getColumnHeaders(filePath)).toEqual(["test_case", "some_value", "some_other_value"]);
   });
 
   [
   { row: 4, column: 'test_case', expected: '4'  },
   ].forEach(({ row, column, expected }) => {
-  test(`The data.csv file has correct value of ${expected} in row ${row} of 'test_case' ${column}`,{tag:'@Test'}, async () => {
+  test(`The data.csv file has correct value of ${expected} in row ${row} of 'test_case' ${column}`,{tag:'@Test'}, () => {
     expect(getCellValue(filePath, (column), (row))).toEqual((expected));
   });
   });
@@ -38,14 +38,14 @@ test.describe('As a user I want to be able to read downloaded csv files', {
   [
     { row: 2, column: 'some_value', expected: 'value 22'  },
   ].forEach(({ row, column, expected }) => {
-  test(`The data.csv file has correct value of ${expected} in row ${row} of 'some_value' ${column}`, async () => {
+  test(`The data.csv file has correct value of ${expected} in row ${row} of 'some_value' ${column}`,  () => {
     expect(getCellValue(filePath, (column), (row))).toEqual((expected));
   });
   });
 [
   { row: 3, column: 'test_case', expected: '3' }
 ].forEach(({ row, column, expected }) => {
-test(`The data.csv file has correct column of 3  ${expected} in row ${row} of 'test_case' ${column}`,{tag:'@Test'}, async () => {
+test(`The data.csv file has correct column of 3  ${expected} in row ${row} of 'test_case' ${column}`,{tag:'@Test'},  () => {
   expect(getCellValue(filePath, (column), (row))).toEqual((expected));
 });
 });
