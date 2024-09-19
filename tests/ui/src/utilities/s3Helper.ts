14f512dcf92fe6c993d827f6e00466921297dd97
AITR1 <178915313+AITR1@users.noreply.github.com>
2024-09-19 10:41:24 +0100

    DR-1117 Add fixes for static analysis failure

--- tests/ui/src/utilities/s3Helper.ts
+++ tests/ui/src/utilities/s3Helper.ts
@@ -3,14 +3,14 @@ const bucket = `nhse-uec-cm-ui-test-bucket-${process.env.ENV}${process.env.WORKS
 
 export function addObject(sourceFilePath: string, targetFile: string) {
   return require('child_process').execSync(
-    `aws s3api put-object --bucket ${bucket} --key ${targetFile} --body ${sourceFilePath}`
+    `aws s3api put-object --bucket ${bucket} --key ${targetFile} --body ${sourceFilePath}`, { shell: false }
 )};
 
 export function getObject(sourceFile: string, targetFile?: string) {
   if (targetFile === undefined) targetFile = sourceFile;
   try {
     return require('child_process').execSync(
-      `aws s3api get-object --bucket ${bucket} --key ${sourceFile} downloads/${targetFile}`
+      `aws s3api get-object --bucket ${bucket} --key ${sourceFile} downloads/${targetFile}`, { shell: false }
     )
   } catch (error) {
     console.log(error);
@@ -20,7 +20,7 @@ export function getObject(sourceFile: string, targetFile?: string) {
 export function deleteObject(targetFile: string) {
   try {
     return require('child_process').execSync(
-      `aws s3api delete-object --bucket ${bucket} --key ${targetFile}`
+      `aws s3api delete-object --bucket ${bucket} --key ${targetFile}`, { shell: false }
     )
   } catch (error) {
     console.log(error);
