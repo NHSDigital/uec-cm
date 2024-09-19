3015a3e52b048224fbcef2a94ee6dbaa7cdb872e
AITR1 <178915313+AITR1@users.noreply.github.com>
2024-09-19 12:18:18 +0100

    DR-1117 Update to be on one line

--- tests/ui/src/utilities/cloudFront.ts
+++ tests/ui/src/utilities/cloudFront.ts
@@ -1,5 +1,4 @@
 export function getCloudFrontUrl(region: string, env: string, workspace: string) {
   const cp = require('child_process')
-  return cp.execSync(
-  `aws cloudfront list-distributions --query "DistributionList.Items[].{DomainName: DomainName, OriginDomainName: Origins.Items[0].DomainName}[?OriginDomainName=='nhse-uec-cm-` + env + `-front-end` + workspace + `.s3.` + region + `.amazonaws.com'] | [0]"`, { shell: false }).toString();
+  return cp.execSync(`aws cloudfront list-distributions --query "DistributionList.Items[].{DomainName: DomainName, OriginDomainName: Origins.Items[0].DomainName}[?OriginDomainName=='nhse-uec-cm-` + env + `-front-end` + workspace + `.s3.` + region + `.amazonaws.com'] | [0]"`, { shell: false }).toString();
 }
