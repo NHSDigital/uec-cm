96f8cf9ea4e3179fc837dd38298de19002a7dbec
AITR1 <178915313+AITR1@users.noreply.github.com>
2024-09-19 11:07:35 +0100

    DR-1117 Update to fix static analysis failures

--- tests/ui/src/utilities/cloudFront.ts
+++ tests/ui/src/utilities/cloudFront.ts
@@ -1,3 +1,5 @@
-export function getCloudFrontUrl(region: string, env: string, workspace: string) {return require('child_process').execSync(
+export function getCloudFrontUrl(region: string, env: string, workspace: string) {
+  const cp = require('child_process')
+  return cp.execSync(
   `aws cloudfront list-distributions --query "DistributionList.Items[].{DomainName: DomainName, OriginDomainName: Origins.Items[0].DomainName}[?OriginDomainName=='nhse-uec-cm-` + env + `-front-end` + workspace + `.s3.` + region + `.amazonaws.com'] | [0]"`, { shell: false }).toString();
 }
