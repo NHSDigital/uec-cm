fc56a32db077299b3e13f1f5d93cc4ba59eea06c
emab1 <178916340+emab1@users.noreply.github.com>
2024-10-09 16:36:36 +0100

    DR-1113 Update eslint configuration

---
+++ src/frontend/eslint.config.mjs
@@ -0,0 +1,79 @@
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import eslintJsPlugin from "@eslint/js";
+import globals from "globals";
+import react from 'eslint-plugin-react';
+import jest from 'eslint-plugin-jest';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+
+export default [
+  eslintJsPlugin.configs.recommended,
+  react.configs.flat.recommended,
+  jest.configs['flat/recommended'],
+  jsxA11y.flatConfigs.recommended,
+
+  {
+    rules: tsPlugin.configs.recommended.rules,
+  },
+  // Standard config
+  {
+    files: ["src/**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      react,
+      "eslint-plugin-jsx-a11y": jsxA11y,
+    },
+  },
+  // Test config
+  {
+    files: ["src/**/*.test.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      react,
+      "eslint-plugin-jest": jest,
+    },
+  },
+  {
+    ignores: ["coverage/**", "**/*.css", "**/dist/**"],
+  },
+];
--- src/frontend/src/components/forwardrefselect/index.tsx
+++ src/frontend/src/components/forwardrefselect/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Select } from 'nhsuk-react-components';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const ForwardRefSelect = React.forwardRef((props : any, ref : any) => {
+const ForwardRefSelect = React.forwardRef((props : any) => {
     return <Select {...props}  />;
 });
 
