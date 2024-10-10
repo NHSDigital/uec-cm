33ff0c2f074a1444559f3201387cf0ea94485de0
prjh2-nhs <178916511+prjh2-nhs@users.noreply.github.com>
2024-10-10 15:08:11 +0100

    Revert "DR-1117 Update to be on one line"
    This reverts commit 3015a3e52b048224fbcef2a94ee6dbaa7cdb872e.

--- tests/ui/src/utilities/s3Helper.ts
+++ tests/ui/src/utilities/s3Helper.ts
@@ -2,14 +2,17 @@ const bucket = `nhse-uec-cm-ui-test-bucket-${process.env.ENV}${process.env.WORKS
 
 export function addObject(sourceFilePath: string, targetFile: string) {
   const cp = require('child_process');
-  return cp.execSync(`aws s3api put-object --bucket ${bucket} --key ${targetFile} --body ${sourceFilePath}`, { shell: false }
+  return cp.execSync(
+    `aws s3api put-object --bucket ${bucket} --key ${targetFile} --body ${sourceFilePath}`, { shell: false }
 )};
 
 export function getObject(sourceFile: string, targetFile?: string) {
   if (targetFile === undefined) targetFile = sourceFile;
   try {
     const cp = require('child_process');
-    return cp.execSync(`aws s3api get-object --bucket ${bucket} --key ${sourceFile} downloads/${targetFile}`, { shell: false })
+    return cp.execSync(
+      `aws s3api get-object --bucket ${bucket} --key ${sourceFile} downloads/${targetFile}`, { shell: false }
+    )
   } catch (error) {
     console.log(error);
   }
@@ -18,7 +21,9 @@ export function getObject(sourceFile: string, targetFile?: string) {
 export function deleteObject(targetFile: string) {
   try {
     const cp = require('child_process');
-    return cp.execSync(`aws s3api delete-object --bucket ${bucket} --key ${targetFile}`, { shell: false })
+    return cp.execSync(
+      `aws s3api delete-object --bucket ${bucket} --key ${targetFile}`, { shell: false }
+    )
   } catch (error) {
     console.log(error);
   }
