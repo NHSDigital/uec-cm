b4de02b46e587562f6a038dda1f1b6fe2c0a66b9
prjh2-nhs <178916511+prjh2-nhs@users.noreply.github.com>
2024-10-10 15:08:29 +0100

    Revert "DR-1117 Update to fix static analysis failures"
    This reverts commit 96f8cf9ea4e3179fc837dd38298de19002a7dbec.

--- tests/ui/src/utilities/cloudFront.ts
+++ tests/ui/src/utilities/cloudFront.ts
@@ -1,5 +1,3 @@
-export function getCloudFrontUrl(region: string, env: string, workspace: string) {
-  const cp = require('child_process')
-  return cp.execSync(
+export function getCloudFrontUrl(region: string, env: string, workspace: string) {return require('child_process').execSync(
   `aws cloudfront list-distributions --query "DistributionList.Items[].{DomainName: DomainName, OriginDomainName: Origins.Items[0].DomainName}[?OriginDomainName=='nhse-uec-cm-` + env + `-front-end` + workspace + `.s3.` + region + `.amazonaws.com'] | [0]"`, { shell: false }).toString();
 }
--- tests/ui/src/utilities/s3Helper.ts
+++ tests/ui/src/utilities/s3Helper.ts
@@ -1,16 +1,15 @@
+
 const bucket = `nhse-uec-cm-ui-test-bucket-${process.env.ENV}${process.env.WORKSPACE}`;
 
 export function addObject(sourceFilePath: string, targetFile: string) {
-  const cp = require('child_process');
-  return cp.execSync(
+  return require('child_process').execSync(
     `aws s3api put-object --bucket ${bucket} --key ${targetFile} --body ${sourceFilePath}`, { shell: false }
 )};
 
 export function getObject(sourceFile: string, targetFile?: string) {
   if (targetFile === undefined) targetFile = sourceFile;
   try {
-    const cp = require('child_process');
-    return cp.execSync(
+    return require('child_process').execSync(
       `aws s3api get-object --bucket ${bucket} --key ${sourceFile} downloads/${targetFile}`, { shell: false }
     )
   } catch (error) {
@@ -20,8 +19,7 @@ export function getObject(sourceFile: string, targetFile?: string) {
 
 export function deleteObject(targetFile: string) {
   try {
-    const cp = require('child_process');
-    return cp.execSync(
+    return require('child_process').execSync(
       `aws s3api delete-object --bucket ${bucket} --key ${targetFile}`, { shell: false }
     )
   } catch (error) {
