bcb526949ef9ceb2e9783ce9c77059157a2d3867
csimpson24334 <chloe.simpson24@nhs.net>
2024-10-14 12:45:45 +0100

    DR-1158 Added view update summary tests (#169)
    * DR-1158 Added view update summary tests
    
    * DR-1158 Added view update summary tests
    
    * DR-1158 View update summary
    
    * DR-1158 Removed the get prefix
    
    Signed-off-by: csimpson24334 <chloe.simpson24@nhs.net>
    
    ---------
    
    Signed-off-by: csimpson24334 <chloe.simpson24@nhs.net>

--- tests/ui/src/pages/questionnaire-check-your-answers-page.ts
+++ tests/ui/src/pages/questionnaire-check-your-answers-page.ts
@@ -4,11 +4,79 @@ export default class CheckAnswersPage {
     constructor(readonly page: Page) {
         this.page = page;
     }
-    get getGoBackLink(): Locator {
+    getGoBackLink(): Locator {
         return this.page.getByRole('button', { name: 'Go back' });
     }
 
-    get getCheckAnswersHeading(): Locator {
+    getCheckAnswersHeading(): Locator {
         return this.page.getByRole('heading', { name: 'Check your answers' });
+    }
+
+    getBedsideNumberField(): Locator {
+        return this.page.locator("//*[contains(@class, 'nhsuk-table__value')]").first();
+    }
+
+    getBedsideField(): Locator {
+        return this.page.locator("//*[contains(@class, 'nhsuk-table__value')]").nth(0);
+    }
+
+    getBedsAvailableField(): Locator {
+        return this.page.locator("//*[contains(@class, 'nhsuk-table__value')]").nth(1);
+    }
+
+    getRequiredStaffField(): Locator {
+        return this.page.locator("//*[contains(@class, 'nhsuk-table__value')]").nth(2);
+    }
+
+    getPlannedAdmissionField(): Locator {
+        return this.page.locator("//*[contains(@class, 'nhsuk-table__value')]").nth(3);
+    }
+
+    getEcmoField(): Locator {
+        return this.page.locator("//*[contains(@class, 'nhsuk-table__value')]").nth(4);
+    }
+
+    getInvasivelyVentilatedField(): Locator {
+        return this.page.locator("//*[contains(@class, 'nhsuk-table__value')]").nth(5);
+    }
+    getNonInvasivelyVentilatedField(): Locator {
+        return this.page.locator("//*[contains(@class, 'nhsuk-table__value')]").nth(6);
+    }
+    getBedsOccupiedUnderOneField(): Locator {
+        return this.page.locator("//*[contains(@class, 'nhsuk-table__value')]").nth(7);
+    }
+    getBedsOccupiedTwelveField(): Locator {
+        return this.page.locator("//*[contains(@class, 'nhsuk-table__value')]").nth(8);
+    }
+    getBedsOccupiedEighteenField(): Locator {
+        return this.page.locator("//*[contains(@class, 'nhsuk-table__value')]").nth(9);
+    }
+
+    getDischargeField(): Locator {
+        return this.page.locator("//*[contains(@class, 'nhsuk-table__value')]").nth(10);
+    }
+
+    getElectiveField(): Locator {
+        return this.page.locator("//*[contains(@class, 'nhsuk-table__value')]").nth(11);
+    }
+    getRefusedUnplannedField(): Locator {
+        return this.page.locator("//*[contains(@class, 'nhsuk-table__value')]").nth(12);
+    }
+    getPatientsNotDiagnosedField(): Locator {
+        return this.page.locator("//*[contains(@class, 'nhsuk-table__value')]").nth(13);
+    }
+    getPatientsDiagnosedField(): Locator {
+        return this.page.locator("//*[contains(@class, 'nhsuk-table__value')]").nth(14);
+    }
+    getSaveConfirmButton(): Locator {
+        return this.page.getByRole('button', { name: "Save and confirm" });
+    }
+
+    getExitButton(): Locator {
+        return this.page.getByRole('button', { name: "Exit without saving" });
+    }
+
+    getEditButton(): Locator {
+        return this.page.locator("(//a[contains(text(),'Edit')])[1]");
     }
 }
