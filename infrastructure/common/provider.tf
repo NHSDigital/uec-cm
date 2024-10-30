a6608cb146f3ae360726af21f4eeab11ab0a3e4d
Michaela McGrath <michaela.mcgrath1@nhs.net>
2024-10-30 15:56:24 +0000

    DR-1172 Ensure proper validation of aws regions (#177)

--- infrastructure/common/provider.tf
+++ infrastructure/common/provider.tf
@@ -9,7 +9,7 @@ provider "aws" {
   region = var.aws_region
 
   skip_metadata_api_check     = true
-  skip_region_validation      = true
+  skip_region_validation      = false
   skip_credentials_validation = true
 
   skip_requesting_account_id = false
@@ -40,7 +40,7 @@ provider "aws" {
   region = "us-east-1"
 
   skip_metadata_api_check     = true
-  skip_region_validation      = true
+  skip_region_validation      = false
   skip_credentials_validation = true
 
   skip_requesting_account_id = false
