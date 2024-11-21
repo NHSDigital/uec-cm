560d126dd4d4ddcef5176453c63619d84d345179
timrickwood <48654585+timrickwood@users.noreply.github.com>
2024-11-21 12:26:58 +0000

    FDOS-56 Update branch naming and commit message rules

--- scripts/functions/test-git-functions.sh
+++ scripts/functions/test-git-functions.sh
@@ -1,117 +1,220 @@
 #!/bin/bash
+source ./scripts/functions/git-functions.sh
+
+unset BRANCH_NAME
+unset BUILD_COMMIT_MESSAGE
+# where commit message does NOT include jira ref it is added
+BRANCH_NAME=${BRANCH_NAME:-$(git rev-parse --abbrev-ref HEAD)}
+BUILD_COMMIT_MESSAGE=${BUILD_COMMIT_MESSAGE:-"My standard commit message"}
+COMMIT_MESSAGE=$(check_jira_ref "$BRANCH_NAME" "$BUILD_COMMIT_MESSAGE")
+if [[ "$COMMIT_MESSAGE" == "$BUILD_COMMIT_MESSAGE" ]]; then
+  echo Commit message not changed to include jira ref
+  all_pass=1
+else
+  echo "Orignal commit message $BUILD_COMMIT_MESSAGE changed to include jira ref -  $COMMIT_MESSAGE"
+fi
+unset BRANCH_NAME
+unset BUILD_COMMIT_MESSAGE
+# where commit message does include jira ref it is NOT added
+BRANCH_NAME="task/DR_9999_No_such_branch"
+BUILD_COMMIT_MESSAGE=${BUILD_COMMIT_MESSAGE:-"DR-9999 My standard commit message"}
+COMMIT_MESSAGE=$(check_jira_ref "$BRANCH_NAME" "$BUILD_COMMIT_MESSAGE")
+if [[ "$COMMIT_MESSAGE" != "$BUILD_COMMIT_MESSAGE" ]]; then
+  echo "Original commit message $BUILD_COMMIT_MESSAGE changed to $COMMIT_MESSAGE"
+  all_pass=1
+else
+  echo "Orignal commit message $BUILD_COMMIT_MESSAGE unchanged -  $COMMIT_MESSAGE"
+fi
+unset BRANCH_NAME
+unset BUILD_COMMIT_MESSAGE
+
+
 
 
 all_pass=0
-export BRANCH_NAME=task/DPTS-2211_My_valid_branch
-export BUILD_COMMIT_MESSAGE="DR-1 My message takes exactly 72 characters to describe this new commit"
+testcount=1
+
+echo "start valid branch name checks"
+export BRANCH_NAME=task/DPTS_2211_My_valid_branch
+export BUILD_COMMIT_MESSAGE="DR-1 My message takes exactly 100 characters to describe the new commit here on this turbo tabletop"
 /bin/bash ./scripts/githooks/git-branch-commit-msg.sh
 if [[ $? = 1 ]]; then
     all_pass=1
 fi
+unset BRANCH_NAME
+unset BUILD_COMMIT_MESSAGE
+testcount=$((testcount+1))
 export BRANCH_NAME=main
-export BUILD_COMMIT_MESSAGE="DR-1 My message takes exactly 72 characters to describe this new commit"
+export BUILD_COMMIT_MESSAGE="DR-1 My message takes exactly 100 characters to describe the new commit here on this turbo tabletop"
 /bin/bash ./scripts/githooks/git-branch-commit-msg.sh
 if [[ $? = 1 ]]; then
     all_pass=1
 fi
 
-# invalid - jira project ref
-export BRANCH_NAME=task/D-22_My_invalid_branch
+echo start invalid checks
+
+unset BRANCH_NAME
+unset BUILD_COMMIT_MESSAGE
+testcount=$((testcount+1))
+export BRANCH_NAME=hotfix/DPTS_2211_My_valid_branch
+export BUILD_COMMIT_MESSAGE="DR-1 My message takes exactly 100 characters to describe the new commit here on this turbo tabletop"
+/bin/bash ./scripts/githooks/git-branch-commit-msg.sh
+if [[ $? = 0 ]]; then
+echo "Test $testcount failed"
+    all_pass=1
+fi
+
+unset BRANCH_NAME
+unset BUILD_COMMIT_MESSAGE
+testcount=$((testcount+1))
+export BRANCH_NAME=feature/DPTS_2211_My_valid_branch
+export BUILD_COMMIT_MESSAGE="DR-1 My message takes exactly 100 characters to describe the new commit here on this turbo tabletop"
+/bin/bash ./scripts/githooks/git-branch-commit-msg.sh
+if [[ $? = 0 ]]; then
+echo "Test $testcount failed"
+    all_pass=1
+fi
+unset BRANCH_NAME
+unset BUILD_COMMIT_MESSAGE
+testcount=$((testcount+1))
+# invalid - jira project ref -hyphenated
+export BRANCH_NAME=task/DPTS-2211_My_invalid_branch
+export BUILD_COMMIT_MESSAGE="DR-1 My message takes exactly 100 characters to describe the new commit here on this turbo tabletop"
+/bin/bash ./scripts/githooks/git-branch-commit-msg.sh
+if [[ $? = 0 ]]; then
+    all_pass=1
+    echo "Test $testcount failed"
+fi
+unset BRANCH_NAME
+unset BUILD_COMMIT_MESSAGE
+testcount=$((testcount+1))
+# invalid - jira project ref - single letter in ref
+export BRANCH_NAME=task/D_22_My_invalid_branch
 export BUILD_COMMIT_MESSAGE="DR-1 My valid commit message"
 /bin/bash ./scripts/githooks/git-branch-commit-msg.sh
 if [[ $? = 0 ]]; then
   all_pass=1
+  echo "Test $testcount failed"
 fi
-# invalid - jira project ref
+unset BRANCH_NAME
+unset BUILD_COMMIT_MESSAGE
+testcount=$((testcount+1))
+# invalid - jira project ref - no separator in ref
 export BRANCH_NAME=task/DR2_My_invalid_branch
 export BUILD_COMMIT_MESSAGE="DR-1 My valid commit message"
 /bin/bash ./scripts/githooks/git-branch-commit-msg.sh
 if [[ $? = 0 ]]; then
     all_pass=1
+    echo "Test $testcount failed"
 fi
+unset BRANCH_NAME
+unset BUILD_COMMIT_MESSAGE
+testcount=$((testcount+1))
 # invalid - no initial cap
-export BRANCH_NAME=task/DR-2_my_invalid_branch
+export BRANCH_NAME=task/DR_2_my_invalid_branch
 export BUILD_COMMIT_MESSAGE="DR-1 My valid commit message"
 /bin/bash ./scripts/githooks/git-branch-commit-msg.sh
 if [[ $? = 0 ]]; then
     all_pass=1
+    echo "Test $testcount failed"
 fi
+unset BRANCH_NAME
+unset BUILD_COMMIT_MESSAGE
+testcount=$((testcount+1))
 # invalid - jira ref too long
-export BRANCH_NAME=task/DPTS-221111_My_invalid_br
+export BRANCH_NAME=task/DPTS_221111_My_invalid_br
 export BUILD_COMMIT_MESSAGE="DR-1 My valid commit message"
 /bin/bash ./scripts/githooks/git-branch-commit-msg.sh
 if [[ $? = 0 ]]; then
     all_pass=1
+    echo "Test $testcount failed"
 fi
+unset BRANCH_NAME
+unset BUILD_COMMIT_MESSAGE
+testcount=$((testcount+1))
 # invalid - branch name too long
-export BRANCH_NAME=task/DPTS-22111_My_invalid_branch
+export BRANCH_NAME=task/DPTS_22111_My_invalid_branch_plus_more_and_text_here_ove
 export BUILD_COMMIT_MESSAGE="DR-1 My valid commit message"
 /bin/bash ./scripts/githooks/git-branch-commit-msg.sh
 if [[ $? = 0 ]]; then
     all_pass=1
+    echo "Test $testcount failed"
 fi
+unset BRANCH_NAME
+unset BUILD_COMMIT_MESSAGE
+testcount=$((testcount+1))
+echo "end branch name checks"
 
-# invalid comment - no jira ref
-export BRANCH_NAME=task/DPTS-2211_My_valid_branch
-export BUILD_COMMIT_MESSAGE="My invalid commit message"
-/bin/bash ./scripts/githooks/git-commit-msg.sh  "My valid commit message"
-if [[ $? = 0 ]]; then
-    all_pass=1
-fi
+echo "start invalid commit message checks"
 # invalid comment - incomplete jira ref
-export BRANCH_NAME=task/DPTS-2211_My_valid_branch
 export BUILD_COMMIT_MESSAGE="D-1 Invalid commit message"
-/bin/bash ./scripts/githooks/git-commit-msg.sh
+check_git_commit_message "$BUILD_COMMIT_MESSAGE"
 if [[ $? = 0 ]]; then
     all_pass=1
+    echo "Test $testcount failed"
 fi
+unset BUILD_COMMIT_MESSAGE
+testcount=$((testcount+1))
 # invalid comment -jira ref has no hyphen
-export BRANCH_NAME=task/DPTS-2211_My_valid_branch
 export BUILD_COMMIT_MESSAGE="DR1 Invalid commit message"
-/bin/bash ./scripts/githooks/git-commit-msg.sh
+check_git_commit_message "$BUILD_COMMIT_MESSAGE"
 if [[ $? = 0 ]]; then
     all_pass=1
+    echo "Test $testcount failed"
 fi
+unset BUILD_COMMIT_MESSAGE
+testcount=$((testcount+1))
 # invalid comment -jira ref too long
-export BRANCH_NAME=task/DPTS-2211_My_valid_branch
 export BUILD_COMMIT_MESSAGE="DR-111111 invalid commit message"
-/bin/bash ./scripts/githooks/git-commit-msg.sh
+check_git_commit_message "$BUILD_COMMIT_MESSAGE"
 if [[ $? = 0 ]]; then
     all_pass=1
+    echo "Test $testcount failed"
 fi
+unset BUILD_COMMIT_MESSAGE
+testcount=$((testcount+1))
 # invalid comment -no initial cap
-export BRANCH_NAME=task/DPTS-2211_My_valid_branch
 export BUILD_COMMIT_MESSAGE="DR-11 invalid commit message"
-/bin/bash ./scripts/githooks/git-commit-msg.sh
+check_git_commit_message "$BUILD_COMMIT_MESSAGE"
 if [[ $? = 0 ]]; then
     all_pass=1
+    echo "Test $testcount failed"
 fi
-# invalid comment -no space after JIRA ref
-export BRANCH_NAME=task/DPTS-2211_My_valid_branch
+unset BUILD_COMMIT_MESSAGE
+testcount=$((testcount+1))
+# invalid comment - no space after JIRA ref
 export BUILD_COMMIT_MESSAGE="DR-11My invalid commit message"
-/bin/bash ./scripts/githooks/git-commit-msg.sh
+check_git_commit_message "$BUILD_COMMIT_MESSAGE"
 if [[ $? = 0 ]]; then
     all_pass=1
+    echo "Test $testcount failed"
 fi
+unset BUILD_COMMIT_MESSAGE
+testcount=$((testcount+1))
 # invalid comment - min three words
-export BRANCH_NAME=task/DPTS-2211_My_valid_branch
 export BUILD_COMMIT_MESSAGE="DR-11 My message"
-/bin/bash ./scripts/githooks/git-commit-msg.sh  "DR-11 My message"
+check_git_commit_message "$BUILD_COMMIT_MESSAGE"
 if [[ $? = 0 ]]; then
     all_pass=1
+    echo "Test $testcount failed"
 fi
+unset BUILD_COMMIT_MESSAGE
+testcount=$((testcount+1))
 # invalid comment - too long
-export BRANCH_NAME=task/DPTS-2211_My_valid_branch
-export BUILD_COMMIT_MESSAGE="DR-11 My message takes too many characters to describe the commit clearly"
-/bin/bash ./scripts/githooks/git-commit-msg.sh
+export BUILD_COMMIT_MESSAGE="DR-1 My message takes exactly 101 characters to describe the new commit here on this turbo tabletops"
+check_git_commit_message "$BUILD_COMMIT_MESSAGE"
 if [[ $? = 0 ]]; then
     all_pass=1
+    echo "Test $testcount failed"
 fi
 
-if [ $all_pass = 1 ] ; then
+unset BRANCH_NAME
+unset BUILD_COMMIT_MESSAGE
+
+if [[ $all_pass = 1 ]] ; then
   echo one or more tests failed
 else
-  echo all tests passed
+  echo all $testcount tests passed
 fi
 
 exit $all_pass
